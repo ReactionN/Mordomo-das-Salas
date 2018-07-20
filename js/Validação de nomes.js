604a7484a105235402644d4f0bc173e32cb23dc3
António Lima <41445459+antoniolima1@users.noreply.github.com>
2018-07-20 16:58:50 +0100

    Validação dos nomes
    Meti aí a função que faz a validação dos nomes. Depois queria ver com voces o que era suposto acontecer quando o nome é mal metido e assim (tipo o que acontece em termos de programa).
    Essa função retorna "já foste" quando o nome é mal metido no campo associado.

---
+++ js/Validação de nomes.js
@@ -0,0 +1,57 @@
+<script>
+function ValName(hostname)
+{
+var string = (hostname.trim());
+var i;
+var size;
+var cnt;
+var format = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+var compare = " ";
+
+cnt = 0;
+
+size = string.length;
+
+space = 0;
+
+for(i=0 ; i < size ; i++)
+{	
+				 if(isNaN(string[i]) == false)  // existe numero-> erro
+ 						{
+    						cnt++;
+							
+							if(string[i].localeCompare(compare) == 0)
+							{
+								//document.write(i);
+								cnt--;
+								if(space != 1 && i < size-1)
+								{
+									if(format.test(string[i+1]) == true)
+									{
+										cnt++;
+									}
+								}
+							}
+        		}
+				
+				
+				
+				if(format.test(string[i]) == true)
+				{
+					cnt++;
+				}
+	
+	
+	
+}
+
+				document.write(cnt);
+				if(cnt !=0)
+					{
+						document.write("ja foste");
+					}
+}
+
+
+
+</script>
