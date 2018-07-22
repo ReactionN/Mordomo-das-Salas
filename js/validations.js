daad315eb2dce5c5ca3b30bd5c4c249bf7a33892
SaraFilipa <41445251+SaraFilipa@users.noreply.github.com>
2018-07-23 00:39:20 +0100

    Add files via upload
    minor change in host name validation file

---
+++ js/validations.js
@@ -0,0 +1,58 @@
+'use script'
+function valName(){
+var hostname = document.forms["reservationForm"]["host"].value;	
+
+var string = (hostname.trim());
+var i;
+var size;
+var cnt;
+var format = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+var compare = " ";
+
+cnt = 0;
+
+size = string.length;
+
+space = 0;
+
+for(i=0 ; i < size ; i++)
+{	
+				 if(isNaN(string[i]) == false)  // existe numero-> erro
+ 						{
+    						cnt++;
+							
+							if(string[i].localeCompare(compare) == 0)
+							{
+								//document.write(i);
+								cnt--;
+								if( space!= 1 && i < size-1)
+								{
+									if(format.test(string[i+1]) == true)
+									{
+										cnt++;
+									}
+									space++;
+								}
+							}
+        		}
+				
+				
+				
+				if(format.test(string[i]) == true)
+				{
+					cnt++;
+				}
+	
+	
+	
+}
+
+				//document.write(cnt);
+				if(cnt !=0 || space == 0)
+					{
+						//document.write("Nome mal inserido");
+						alert("Nome mal inserido");
+						return false;
+					}
+}
+
